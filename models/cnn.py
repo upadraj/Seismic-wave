e595cc0681b43e5b996ca4e5fa835dbce25d91e2
Upadhayaya, Rajesh <upadraj@gatech.edu>
2024-09-09 12:10:14 -0600

    Z norm  added.

--- models/cnn.py
+++ models/cnn.py
@@ -6,29 +6,21 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=2, padding=1)
-        # self.bn1 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=2, padding=1)    
         self.conv2 = nn.Conv2d(
             in_channels=128, out_channels=256, kernel_size=2, padding=1
         )
-        # self.bn2 = nn.BatchNorm2d(64)
-
         self.conv3 = nn.Conv2d(
             in_channels=256, out_channels=512, kernel_size=2, padding=1
         )
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        # self.bn3 = nn.BatchNorm2d(128)
-
-        # self.`dropout` = nn.Dropout(p=0.25)
-
         self.fc1 = nn.Linear(512 * 27 * 27, 512)
-        # self.bn_fc1 = nn.BatchNorm1d(256)
+        
         self.fc2 = nn.Linear(512, 256)
-        # self.bn_fc2 = nn.BatchNorm1d(128)
         self.fc3 = nn.Linear(256, 6)
 
-    def forward(self, x):
+    def forward(self, x):        
         x = F.tanh(self.conv1(x))
         x = self.pool(F.tanh((self.conv2(x))))
         x = F.tanh((self.conv3(x)))
